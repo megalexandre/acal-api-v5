e46b1c84c17afc1f7cb5c6c72973c474a70f275f
Alexandre Queiroz <megalexandre@gmail.com>
2024-02-27 09:39:24 -0300

    add -> ... first cucumber test

--- src/test/java/br/org/acal/apicore/steps/MyStepdefs.kt
+++ src/test/java/br/org/acal/apicore/steps/MyStepdefs.kt
@@ -2,17 +2,22 @@ package br.org.acal.apicore.steps
 
 import io.cucumber.java.en.Then
 import io.cucumber.java.en.When
+import io.restassured.response.Response
+import org.apache.http.HttpStatus.SC_OK
+import org.junit.jupiter.api.Assertions.assertEquals
 
 class MyStepdefs: SpringIT() {
 
+    var response: Response? = null
+
     @When("I send a GET request to {string}")
     fun iSendAGETRequestTo(url: String) {
-
+        response = executeGet(url)
     }
 
     @Then("the response status code should be {int}")
     fun theResponseStatusCodeShouldBe(status: Int) {
-
+        assertEquals(SC_OK,response?.statusCode)
     }
 
 }
---
+++ src/test/java/br/org/acal/apicore/steps/SpringIT.kt
@@ -0,0 +1,101 @@
+package br.org.acal.apicore.steps
+
+import br.org.acal.apicore.ApiCoreApplication
+import br.org.acal.apicore.resources.repository.CategoryRepository
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import io.cucumber.spring.CucumberContextConfiguration
+import io.restassured.RestAssured
+import io.restassured.builder.RequestSpecBuilder
+import io.restassured.module.kotlin.extensions.Given
+import io.restassured.module.kotlin.extensions.When
+import io.restassured.response.Response
+import io.restassured.specification.RequestSpecification
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.BeforeEach
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT
+import org.springframework.boot.test.web.server.LocalServerPort
+import org.testcontainers.containers.GenericContainer
+import org.testcontainers.junit.jupiter.Container
+import org.testcontainers.junit.jupiter.Testcontainers
+
+@Testcontainers
+@SpringBootTest(webEnvironment = RANDOM_PORT, classes = [ApiCoreApplication::class])
+@CucumberContextConfiguration
+class SpringIT {
+
+    @Autowired
+    private lateinit var categoryRepository: CategoryRepository
+
+    companion object {
+        private const val USERNAME = "root"
+        private const val PASSWORD = "example"
+
+        @Container
+        val mongoContainer: GenericContainer<*> = GenericContainer<Nothing>("mongo:4.0.10").apply {
+            withExposedPorts(27017)
+            withReuse(true)
+            withEnv("MONGO_INITDB_ROOT_USERNAME", USERNAME)
+            withEnv("MONGO_INITDB_ROOT_PASSWORD", PASSWORD)
+        }
+
+        @BeforeAll
+        @JvmStatic
+        fun setUp() {
+            mongoContainer.start()
+        }
+
+        /*
+        @DynamicPropertySource
+        @JvmStatic
+        fun properties(registry: DynamicPropertyRegistry) {
+            registry.add("spring.data.mongodb.uri") {
+                "mongodb://${USERNAME}:${PASSWORD}@${mongoContainer.host}:${
+                    mongoContainer.getMappedPort(
+                        27017
+                    )
+                }/"
+            }
+        }
+         */
+    }
+
+
+    @LocalServerPort
+    private var serverPort = -1;
+
+    @BeforeEach
+    fun setup(){
+        RestAssured.port = serverPort
+        resetDatabase()
+    }
+
+    private fun buildRequestSpec(): RequestSpecification {
+        return RequestSpecBuilder()
+            .addHeader("Content-Type", "application/json")
+            .build()
+    }
+    fun resetDatabase(){
+        categoryRepository.deleteAll()
+    }
+
+    val gson: Gson = GsonBuilder()
+        //.registerTypeAdapter(LocalDate::class.java, LocalDateTypeAdapter())
+        //.registerTypeAdapter(LocalDateTime::class.java, LocalDateTimeTypeAdapter())
+        .create()
+
+
+    fun executeGet(url: String): Response {
+        val header = mutableMapOf<String,String>()
+        header["Content-Type"] = "application/json"
+
+        return Given {
+            headers(header)
+        } When {
+            get("http://localhost:$serverPort/todo")
+        }
+    }
+
+}
