e2d7197b570dbf346d5f37ef84b1a285f7027c4a
Alexandre Queiroz <megalexandre@gmail.com>
2024-03-05 15:00:40 -0300

    add --> invoiceTest

--- src/test/kotlin/br/org/acal/apicore/domain/entity/InvoiceNumberTest.kt
+++ src/test/kotlin/br/org/acal/apicore/domain/entity/InvoiceNumberTest.kt
@@ -9,6 +9,21 @@ import stub.invoiceNumberStub
 class InvoiceNumberTest{
 
     @Test
+    fun `when create a invoice number by string should values be correctly`(){
+
+        val invoice = InvoiceNumber.of("2000.01.1")
+
+        assertEquals(2000, invoice.year.value)
+        assertEquals(1, invoice.month.value)
+        assertEquals("1", invoice.number)
+
+        assertEquals("2000.01.000001", invoice.value)
+        assertEquals(Reference.of("2000.1"), invoice.reference)
+    }
+
+
+
+    @Test
     fun test(){
         val firstJanuaryOf2000 = invoiceNumberStub.copy(
             year = Year.of(2000),
